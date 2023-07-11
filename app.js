dc0c622354fc8f32fb59edd6ba115a617d226e90
joe-bor <kimborlongan_08@yahoo.com>
2023-07-11 16:26:01 -0700

    2nd

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const input = document.getElementById("number");
 
 addBtn.addEventListener("click", () => {
   counter += parseInt(input.value);
+  counter > 0
+    ? (countDisplay.style.color = "black")
+    : (countDisplay.style.color = "red");
   countDisplay.innerText = counter;
   console.log(counter);
 });
 
 subtractBtn.addEventListener("click", () => {
   counter -= parseInt(input.value);
+  counter > 0
+    ? (countDisplay.style.color = "black")
+    : (countDisplay.style.color = "red");
   countDisplay.innerText = counter;
   console.log(counter);
 });
